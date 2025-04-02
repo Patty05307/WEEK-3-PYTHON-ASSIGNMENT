c87f8ddf919f157c810491d0632827bfdf192d33
Patty05307 <ibitokunpatience05@gmail.com>
2025-04-02 15:01:00 +0100

    Python week 3 Assignment solution

---
+++ function.py
@@ -0,0 +1,6 @@
+# Test the function with given values
+test_price = 10000
+test_discount = 70
+
+# Calculate and print the final price
+print("Final price after applying discount: $(10,000, 70);2f}")
